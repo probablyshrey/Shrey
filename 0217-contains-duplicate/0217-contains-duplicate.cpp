ed810d489eb2f4bce3facb93e2e19055ac7fbaa7
Shrey Jaiswal <shreyyjaiswal69@gmail.com>
2026-09-08 14:29:00 +0530

    Time: 79 ms (28.71%), Space: 111.3 MB (62.9%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> m;
+        for (int i=0; i<nums.size(); i++)
+        {
+            if (m.find(nums[i])!=m.end())
+            {
+                return true;
+            }
+            else
+            {
+                m.emplace(nums[i]);
+            }
+        }
+        return false;
+    }
+};
