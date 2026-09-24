033ef9eb17978e1a2d989298645c64a65271c39a
Shrey Jaiswal <shreyyjaiswal69@gmail.com>
2026-09-25 01:50:44 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    int bin(vector<int>& a, int target,int low,int high)
+    {
+        int mid=low+(high-low)/2;
+        if (low>high) return -1;
+        if (a[mid]==target) return mid;
+        if (a[mid]<target)
+        {
+            return bin(a, target, mid+1, high);
+        }
+        if (a[mid]>target)
+        {
+            return bin(a, target, low, mid-1);
+        }
+        return -1;
+    }
+    int leftright(vector<int>& a,int target, int low, int high)
+    {
+        int mid=low+(high-low)/2;
+        if (a[mid]==target) return mid;
+        if (low>high) return -1;
+
+        if (a[low]<=a[mid])
+        {
+            if (target >= a[low] && target < a[mid]){
+                return bin(a, target, low, high);
+            }
+            else{
+                return leftright(a, target, mid+1, high);
+            }
+        }
+        else if(a[high]>a[mid])
+        {
+            if (target > a[mid] && target <= a[high])
+            {
+                return bin(a, target, mid+1, high);
+            }
+            else{
+                return leftright(a, target, low, mid-1);
+            }
+        }
+        return -1;
+    }
+    int search(vector<int>& nums, int target) {
+        return leftright(nums, target, 0, nums.size()-1);
+    }
+};
